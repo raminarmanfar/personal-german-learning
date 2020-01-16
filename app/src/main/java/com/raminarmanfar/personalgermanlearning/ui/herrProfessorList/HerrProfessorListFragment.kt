f53ab1248f497d82b7f083f36a97fba48f8b5c8b
armanfar <Ramin.Armanfar@msg.group>
2020-01-16 10:56:54 +0100

    Main menu buttons created and onClick events linked to the fragments.

--- app/src/main/java/com/raminarmanfar/personalgermanlearning/ui/herrProfessorList/HerrProfessorListFragment.kt
+++ app/src/main/java/com/raminarmanfar/personalgermanlearning/ui/herrProfessorList/HerrProfessorListFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.raminarmanfar.personalgermanlearning.R
 
@@ -22,10 +20,12 @@ class HerrProfessorListFragment : Fragment() {
         herrProfessorListViewModel =
             ViewModelProviders.of(this).get(HerrProfessorListViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_herr_professor_list, container, false)
+        /*
         val textView: TextView = root.findViewById(R.id.text_herr_professor_list)
         herrProfessorListViewModel.text.observe(this, Observer {
             textView.text = it
         })
+        */
         return root
     }
 }
--- app/src/main/java/com/raminarmanfar/personalgermanlearning/ui/home/HomeFragment.kt
+++ app/src/main/java/com/raminarmanfar/personalgermanlearning/ui/home/HomeFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.raminarmanfar.personalgermanlearning.R
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private lateinit var homeViewModel: HomeViewModel
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +31,25 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.btnHerrProfessorList).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btnPersonalDictionary).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btnProfile).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btnTools).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btnAbout).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.btnHerrProfessorList -> navController!!.navigate(R.id.action_nav_home_to_nav_herr_professor_list)
+            R.id.btnPersonalDictionary -> navController!!.navigate(R.id.action_nav_home_to_nav_personal_dictionary)
+            R.id.btnProfile -> navController!!.navigate(R.id.action_nav_home_to_nav_profile)
+            R.id.btnTools -> navController!!.navigate(R.id.action_nav_home_to_nav_tools)
+            R.id.btnAbout -> navController!!.navigate(R.id.action_nav_home_to_nav_about)
+        }
     }
 }
